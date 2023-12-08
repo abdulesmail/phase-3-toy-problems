353cea55512ca44dd4c3a0c8cf52eeb744c1fef9
abdulesmail <abdul.esmail@student.moringaschool.com>
2023-12-08 19:44:50 +0300

    completed assignment

---
+++ clock.py
@@ -0,0 +1,16 @@
+def convert_to_24_hour_format(hour, minute, period):
+    if period.lower() == "am":
+        if hour == 12:
+            hour = 0
+    else:
+        if hour != 12:
+            hour += 12
+
+    return "{:02d}:{:02d}".format(hour, minute)
+
+# Example usage:
+hour = 6
+minute = 40
+period = "pm"
+result = convert_to_24_hour_format(hour, minute, period)
+print(result)
