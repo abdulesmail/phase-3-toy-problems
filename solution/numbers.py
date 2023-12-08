3ddf4d369879ac66c4c4051a72cd61212128b413
abdulesmail <abdul.esmail@student.moringaschool.com>
2023-12-08 21:37:55 +0300

    completed assignment

---
+++ solution/numbers.py
@@ -0,0 +1,18 @@
+def exactly_two_positive(a, b, c):
+    positive_count = 0
+
+    if a > 0:
+        positive_count += 1
+    if b > 0:
+        positive_count += 1
+    if c > 0:
+        positive_count += 1
+
+    return positive_count == 2
+
+print(exactly_two_positive(5, -9, 7))
+print(exactly_two_positive(3, 6, 9))  
+print(exactly_two_positive(8, 5, -9))   
+print(exactly_two_positive(2, 6, -3))   
+print(exactly_two_positive(-6, -3, 8))  
+print(exactly_two_positive(9, -7, -4))
